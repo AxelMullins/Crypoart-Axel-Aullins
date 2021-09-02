b1c1efe675b88112fa7165204351a0b46f3b4318
Axel Mullins <axelmullins94@gmail.com>
2021-09-02 15:20:19 -0300

    Ecommerce de NFT

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './nftlogo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          eCommerce de NFT (sin estar dentro de la BlockChain)
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.larvalabs.com/cryptopunks"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Caso similar
         </a>
       </header>
     </div>
